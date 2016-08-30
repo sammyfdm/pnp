6db9a9cea1b024eeb5bfbc9139581ba3e27384c3
sammydm <oikimono@gmail.com>
2016-08-30 18:27:37 -0400

    multiple rolls

--- pnp.py
+++ pnp.py
@@ -1,4 +1,4 @@
-def roll(sides, dice=1):
+def roll(sides, dice=1, rolls=1):
 	import random
-	return sum([random.randint(1, sides) for i in range(dice)])
+	return [sum([random.randint(1, sides) for i in range(dice)]) for j in range(rolls)]
 
